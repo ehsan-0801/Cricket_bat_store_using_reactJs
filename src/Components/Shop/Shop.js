3d2f0b6692e600319b343483121045345ecee731
ehsan-0801 <ehsanulahmmed@gmail.com>
2022-03-26 16:48:52 +0600

    add to cart problem solved

--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { useEffect, useState } from 'react';
 import Product from '../Product/Product';
 import './Shop.css'
-
+import Alert from 'react-bootstrap/Alert'
+import { Button } from 'react-bootstrap';
 const Shop = () => {
     const [products, setProducts] = useState([]);
     const [cart, setCart] = useState([]);
-    console.log(cart)
      useEffect(()=>{
          fetch('product.json')
          .then(res => res.json())
          .then(data => setProducts(data))
      },[]) 
+     
      const handleAddToCart = (product)=>{
         const newcart = [...cart, product];
-        setCart(newcart);
+        const uniqeArray = newcart.filter((item, index) => newcart.indexOf(item) === index);
+        if(uniqeArray.length <= 4){
+            setCart(uniqeArray);
+            console.log(uniqeArray)
+        }
+        else{
+        }
+        
      }
     return (
         <div className="bg-custom">
@@ -30,16 +38,17 @@ const Shop = () => {
                 </div>
                 <div className="cart_container col-md-3 col-sm-12 ">
                     <div>
+                        <h3 className='fw-bold text-primary mb-5'>Selected Products</h3>
                         {
                             cart.map((product)=>(
-                                <div className="bg-light p-1 border border-1 rounded my-1">
-                                    <img className='cartimg' src={product.image} alt='cartimg'/>
-                                     <h6 className="" key={product.id}>{product.name} </h6>
+                                <div key={product.id} className="row bg-light p-1 border border-1 rounded my-1">
+                                    <img className='cartimg col-md-2' src={product.image}  alt='cartimg'/>
+                                     <h6 className="text-primary col-md-10" >{product.name} </h6>
                                 </div>
-                               
-                                
                             ))
                         }
+                        <button className='btn btn-primary px-5'>Choose 1 For me</button>
+                        <button className='btn btn-danger px-5'>Choose Again</button>
                     </div>
                     
                 </div>
