409ba1fabe78136e23b616c09ae5f4a87e3f23b8
ehsan-0801 <ehsanulahmmed@gmail.com>
2022-03-26 14:53:28 +0600

    add to cart done

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './Product.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 const Product = (props) => {
+    const {handleAddToCart, product} = props;
     const {name, image, price} = props.product;
     return (
         <div className='product'>
@@ -9,8 +12,9 @@ const Product = (props) => {
                 <h3 className='product_name'>{name}</h3>
                 <p>Price: {price} Taka(BDT)</p>
             </div>
-            <button className='btn-cart'>
-                <p className='btn_text fs-4 fw-bold'>Add to Cart </p>
+            <button onClick={() =>handleAddToCart(product)} className='btn-cart'>
+                <p className='btn_text fs-5 fw-bold'>Add to Cart </p>
+                <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
             </button>
         </div>
     );
--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -5,11 +5,17 @@ import './Shop.css'
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([]);
+    console.log(cart)
      useEffect(()=>{
          fetch('product.json')
          .then(res => res.json())
          .then(data => setProducts(data))
      },[]) 
+     const handleAddToCart = (product)=>{
+        const newcart = [...cart, product];
+        setCart(newcart);
+     }
     return (
         <div className="bg-custom">
             <div className="row">
@@ -18,11 +24,24 @@ const Shop = () => {
                      products.map(product =><Product
                      key={product.id}
                      product={product}
+                     handleAddToCart={handleAddToCart}
                      ></Product>)
                  }
                 </div>
                 <div className="cart_container col-md-3 col-sm-12 ">
-                    <h4 className="text-danger">Order Summary</h4>
+                    <div>
+                        {
+                            cart.map((product)=>(
+                                <div className="bg-light p-1 border border-1 rounded my-1">
+                                    <img className='cartimg' src={product.image} alt='cartimg'/>
+                                     <h6 className="" key={product.id}>{product.name} </h6>
+                                </div>
+                               
+                                
+                            ))
+                        }
+                    </div>
+                    
                 </div>
             </div>
         </div>
